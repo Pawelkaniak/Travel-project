2b63086cba5b602fc24a440dd0b21cb100a5d593
Pawel Kaniak <pawkaniak@gmail.com>
2017-03-30 00:35:31 +0200

    Change class in 'membership' section with javascript

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,7 +1,37 @@
-/*(function() {
-   funkcja do zabezpieczenia kodu(wyłączam ją :o)*/
-  console.log('dziala');
+document.addEventListener("DOMContentLoaded", function () {
+  /*funkcja do zabezpieczenia kodu*/
+  console.log('DOM fully loaded and parsed');
+
+  
+  
+  /*  */ 
+  
+  
+  /* datapicker in JQuery */
+  $(function() {
+    $( "#datepicker" ).datepicker();
+  } );
 
 
-//})();
+
+ /* JS changing class in membership section.  */
+  var buttons = document.querySelectorAll('button.join_us');
+  var oldClass = document.querySelectorAll('.support_member');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (event) {
+      if (oldClass[i].classList.contains('support_member')) {
+        oldClass[i].classList.remove('support_member');
+        oldClass[i].classList.add('color_member');
+      }
+      else {
+        oldClass[i].classList.remove('color_member');
+        oldClass[i].classList.add('support_member');
+      }
+    });
+  }
+ 
+});
+
+
+
 
