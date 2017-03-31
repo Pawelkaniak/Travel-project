6e9e5a6b544c7e01f913d3122908fdbfac2bc719
Pawel Kaniak <pawkaniak@gmail.com>
2017-04-01 00:17:47 +0200

    add JQuery animation for dropdown-list

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,20 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-  /*funkcja do zabezpieczenia kodu*/
   console.log('DOM fully loaded and parsed');
+  /*funkcja do zabezpieczenia kodu*/
+  
 
+  /* Animation for account - dropdown list JS */
   
   
-  /*  */ 
+  /* var buttonSingIn = document.querySelector('#colorSi');
+   var dropDownList = document.querySelector('.dropdown');
+   
+   console.log(buttonSingIn);
+   
+   buttonSingIn.addEventListener("click", function (event) {
+     if (dropDownList.style.visibility == 'visible') {
+       dropDownList.style.visibility = 'hidden';
+     } else { 
+       dropDownList.style.visibility = 'visible';
+       }
+   }); */
+  
+  
+  /* Animation for account - dropdown list JQuery */
+
+    /* slide down animation JQ*/
+  $('#colorSi').on('click', function () {
+    $('.dropdown').find('li').eq(0).slideToggle(250);
+    $('.dropdown').find('li').eq(1).slideToggle(500);
+    $('.dropdown').find('li').eq(2).slideToggle(750);
+    $('.dropdown').find('li').eq(3).slideToggle(1000);
+  });
+  
+  /*$('#colorSi').on('click', function () {
+    $('.dropdown').find('li').eq(0).slideToggle(250, function()  { 
+      $('.dropdown').find('li').eq(1).slideToggle(250, function()  { 
+        $('.dropdown').find('li').eq(2).slideToggle(250, function()  { 
+          $('.dropdown').find('li').eq(3).slideToggle(250); 
+        })
+      })
+    })
+  });*/
+      
+      /*$('.dropdown').find('li').eq(2).slideToggle(1000);
+    $('.dropdown').find('li').eq(3).slideToggle(1000);
+    });*/
   
   
   /* datapicker in JQuery */
-  $(function() {
-    $( "#datepicker" ).datepicker();
-  } );
-
-
-
- /* JS changing class in membership section.  */
+  $(function () {
+    $("#datepicker").datepicker();
+  });
+  
+  
+  /* JS changing class in membership section.  */
   var buttons = document.querySelectorAll('button.join_us');
   var oldClass = document.querySelectorAll('.support_member');
   for (let i = 0; i < buttons.length; i++) {
@@ -29,9 +66,4 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
- 
 });
-
-
-
-
